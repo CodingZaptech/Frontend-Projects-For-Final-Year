07684fac26cd012ade4abce06f022bc6c7078582
CodingZap <help@codingzap.com>
2025-10-01 15:34:21 +0530

    Create script.js

---
+++ Redesign An Existing Website/script.js
@@ -0,0 +1,6 @@
+// Button click event
+const learnMoreBtn = document.getElementById("learnMoreBtn");
+
+learnMoreBtn.addEventListener("click", () => {
+  alert("Thank you for exploring our redesigned website!");
+});
